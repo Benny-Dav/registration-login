122d2b896117165f8e8730e516c2e5e5fcbe0d94
Benedicta Davour <benedictadavour777@gmail.com>
2024-09-17 13:59:47 -0700

    added some css and more javasript

--- scripts/register.js
+++ scripts/register.js
@@ -1,11 +1,27 @@
 // Select form by id
-const registerForm = document.getElementById("register-form");
+const registerForm = document.querySelector("#register-form");
+console.log(registerForm);
+
 
 // Handle form submit event
-registerForm.onsubmit = function() {
-//   Collect username
-// Collect email
-// Collect password
-// Save user information
-// Send confirmation email
+registerForm.addEventListener("submit", function () {
+    //   Collect username
+    // Collect email
+    // Collect password
+    // Save user information
+    // Send confirmation email
+});
+console.log(squareOfAge);
+
+// AGE VARIABLE
+const age = 44;
+
+// SQUARE OF AGE
+function squareOfAge(age) {
+    const squaredAge = age ** 2;
+    return squaredAge;
 }
+
+console.log(squareAge(age));
+const squaredAge = squareAge(45);
+console.log(squaredAge)
