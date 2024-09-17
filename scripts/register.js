2f895c43d3fea6b423fdff7bd15b24ad30921767
Benedicta Davour <benedictadavour777@gmail.com>
2024-09-17 08:22:35 -0700

    fixed register-form id in js

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,5 @@
 // Select form by id
-const registerForm = document.getElementById("register.form");
+const registerForm = document.getElementById("register-form");
 
 // Handle form submit event
 registerForm.onsubmit = function() {
